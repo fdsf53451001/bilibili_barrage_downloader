e621fcf60bb2cd233e9e602d9324c8e16cb2915f
Toby Ya <35889113+fdsf53451001@users.noreply.github.com>
2022-03-01 02:12:00 +0800

    update

--- download.py
+++ download.py
@@ -1,23 +1,44 @@
+from matplotlib.pyplot import bar
 import requests
 from bs4 import BeautifulSoup
 import json
 
-vid = 'BV1os411D7be'
-# vid = 'BV1Q3411Y7Z6'
+bvid = 'BV1os411D7be'
+# bvid = 'BV1Q3411Y7Z6'
 
-if __name__ == "__main__":
-    video_info = requests.get("https://api.bilibili.com/x/player/pagelist?bvid="+vid+"&jsonp=jsonp")
+def get_video_cid(bvid):
+    video_info = requests.get("https://api.bilibili.com/x/player/pagelist?bvid="+bvid+"&jsonp=jsonp")
+    print('get cid',video_info.status_code)
     video_info = BeautifulSoup(video_info.text, "html.parser")
     video_info_json = json.loads(video_info.text)
     cid = video_info_json['data'][0]['cid']
+    return cid
 
+def get_barrage(cid):
     barrage_info = requests.get("https://api.bilibili.com/x/v1/dm/list.so?oid="+str(cid))
+    print('get barrage',barrage_info.status_code)
     barrage_info.encoding = 'utf-8'
-    # print(barrage_info.text)
     barrage_info = BeautifulSoup(barrage_info.text, "xml")
+    # print(barrage_info)
     barrages = barrage_info.find_all('d')
+    result = []
     for barrage in barrages:
         # print(barrage['p'],barrage.get_text())
         single_barrage_info = barrage['p'].split(',')
         single_barrage_text = barrage.get_text()
-        print(single_barrage_info[0],single_barrage_text)
+        result.append([single_barrage_info[0],single_barrage_text])
+    return result
+
+def save_barrage(bvid,barrage):
+    with open(bvid+'_barrage.txt','w') as file:
+        for single_barrage in barrage:
+            file.write(single_barrage[0]+' "'+single_barrage[1]+'"\n')
+    return
+
+if __name__ == "__main__":
+    cid = get_video_cid(bvid)
+    print('cid : ',cid)
+    barrage = get_barrage(cid)
+    save_barrage(bvid,barrage)
+
+    
